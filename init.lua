5dcbd401f2357d2c862a1d95549647149c904af6
Michael Hall <mhaldini@icloud.com>
2026-05-05 11:56:12 +1000

    change line number colours

--- init.lua
+++ init.lua
@@ -104,6 +104,9 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+-- Enable true colour support for themes
+vim.opt.termguicolors = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
@@ -1050,24 +1053,27 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        style = 'night',
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = false },
         },
+        on_highlights = function(hl, c)
+          local mid_grey = '#7a88b5'
+          -- Brighten the relative numbers above and below the cursor
+          hl.LineNrAbove = { fg = mid_grey, bold = false }
+          hl.LineNrBelow = { fg = mid_grey, bold = false }
+
+          -- Fallback just in case you ever disable relative numbers
+          hl.LineNr = { fg = c.fg_dark, bold = true }
+        end,
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
